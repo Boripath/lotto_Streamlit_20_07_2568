4a860ba63a55675162577c5debb62a8c01cddd2d
Boripath <yeenboripath@gmail.com>
2025-07-20 18:37:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from _1_header import render_header
 from _2_pricerate import select_pricerate
 from _3_bet_type import bet_type_selector
+from _4_input_number import input_numbers
 
 st.set_page_config(page_title="โพยหวยออนไลน์", layout="centered")
 
@@ -13,3 +14,6 @@ rate = select_pricerate()
 
 # ✅ เลือกประเภทการแทง
 bet_type, double_mode = bet_type_selector()
+
+# ✅ ใส่ตัวเลข
+numbers = input_numbers(bet_type)
