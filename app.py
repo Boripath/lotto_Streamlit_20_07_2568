8f15b002e26f222ff74aecaa0a3fdb4a3ad6f925
Boripath <yeenboripath@gmail.com>
2025-07-20 21:18:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from _2_pricerate import select_pricerate
 from _3_bet_type import bet_type_selector
 from _4_input_number import input_numbers
 from _5_input_price import input_price
+from _6_bill_table import show_bill_table
 st.set_page_config(page_title="โพยหวยออนไลน์", layout="centered")
 
 # ✅ แสดงหัวเว็บ
@@ -20,3 +21,6 @@ numbers = input_numbers(bet_type, double_mode)
 
 # ✅ ใส่ราคา
 input_price(numbers, bet_type)
+
+# ✅ แสดงบิล
+show_bill_table()
