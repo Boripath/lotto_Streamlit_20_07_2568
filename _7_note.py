225c3dcc348b03fcbcae1a432b75112d17600472
Boripath <yeenboripath@gmail.com>
2025-07-20 21:46:12 +0700

    Create _7_note.py

---
+++ _7_note.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def show_note_and_total():
+    if "bills" in st.session_state and st.session_state.bills:
+        total_top = sum(b.get("top", 0) for b in st.session_state.bills)
+        total_bottom = sum(b.get("bottom", 0) for b in st.session_state.bills)
+        total_amount = total_top + total_bottom
+        st.markdown(f"<h4>ยอดรวม (บาท): {total_amount}</h4>", unsafe_allow_html=True)
+
+    st.text_area("บันทึกช่วยจำ", key="memo")
