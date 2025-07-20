452fb01f974041fa74efd04dbc2f11e2e44b8d82
Boripath <yeenboripath@gmail.com>
2025-07-20 15:52:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from _1_header import render_header
 from _2_pricerate import select_pricerate
+from _3_bet_type import select_bet_type
 
 st.set_page_config(page_title="โพยหวยออนไลน์", layout="centered")
 
@@ -9,3 +10,6 @@ render_header()
 
 # ✅ เลือกอัตราจ่าย
 rate = select_pricerate()
+
+# ✅ เลือกประเภทการแทง
+selected_bet_type = select_bet_type()
