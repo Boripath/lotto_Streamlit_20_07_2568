aefd091793833f85b4559bb6989153955804e10a
Boripath <yeenboripath@gmail.com>
2025-07-20 21:51:34 +0700

    Update _7_note.py

--- _7_note.py
+++ _7_note.py
@@ -5,6 +5,11 @@ def show_note_and_total():
         total_top = sum(b.get("top", 0) for b in st.session_state.bills)
         total_bottom = sum(b.get("bottom", 0) for b in st.session_state.bills)
         total_amount = total_top + total_bottom
-        st.markdown(f"<h4>ยอดรวม: {total_amount}</h4> (บาท)", unsafe_allow_html=True)
 
-    st.text_area("บันทึกช่วยจำ", key="memo")
+        st.markdown(f"<h4 style='display:inline;'>ยอดรวม: {total_amount}</h4> <span>(บาท)</span>", unsafe_allow_html=True)
+
+    col1, col2 = st.columns([1, 5])
+    with col1:
+        st.markdown("<b>บันทึกช่วยจำ</b>", unsafe_allow_html=True)
+    with col2:
+        st.text_area("", key="memo", height=50)
